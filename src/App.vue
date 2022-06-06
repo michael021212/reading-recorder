ce2e2503db3881e4fd1323715d558c10f5b108d5
tanakashun <michael021212@gmail.com>
2022-06-07 00:44:36 +0900

    トップ画面

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Reading Recorder</h2>
+    <!-- メインメニューを定義 -->
+    <el-menu mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1">
+        <router-link to="/">
+          Home
+        </router-link>
+      </el-menu-item>
+      <!-- サブメニューを定義 -->
+      <el-submenu index="3">
+        <template v-slot:title>Support</template>
+        <el-menu-item index="3-1">
+          <a href="https://wings.msn.to/" target="help">Author</a>
+        </el-menu-item>
+        <el-menu-item index="3-2">
+          <a href="https://www.sbcr.jp/" target="help">Publisher</a>
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
+    <!-- ルーティング時の表示領域を準備 -->
+    <route-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import BookSearch from '../components/BookSearch.vue'
+import BookForm from '../components/BookForm.vue'
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
@@ -17,6 +19,20 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: BookSearch
+  },
+  {
+    path: '/form',
+    name: 'form',
+    component: BookForm
+  },
+  {
+    path: '*',
+    redirect: '/'
   }
 ]
 
